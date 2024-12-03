166fe1dc3fdf921b3f293a220b28b626555a1483
Sanjay Kandula <sanjaykandula3@gmail.com>
2024-12-03 22:15:38 +0530

    profile commit

--- src/myprofile.js
+++ src/myprofile.js
@@ -8,7 +8,7 @@ const Myprofile = () => {
     const [token,setToken] = useContext(store);
     const [data,setData] = useState(null);
     useEffect(() =>{
-        axios.get('http://localhost:5000/myprofile',{
+        axios.get('https://loginauthen-mern.onrender.com/myprofile',{
             headers: {
                 'x-token' : token
             }
